01e379c1d04c9a1150c436c0e206ddc34436ed05
Luskahz <lucasalvesdemelolins@gmail.com>
2025-05-02 22:00:55 -0300

    base da api

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = 3000
+
+
+app.use(cors)
+app.use(express.json())
+
+//Rotas
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`\n \n Servidor do crl \n\nrodando na porra da porta ${port}`)
+})
