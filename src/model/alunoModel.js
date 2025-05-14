1190129436ff390e5c1d3765373f07cbcd426316
João Beserra <joao.beserra@aluno.ifsp.edu.br>
2025-05-13 22:12:01 -0300

    criação crud aluno

---
+++ src/model/alunoModel.js
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export const findAll = () => prisma.alunos.findMany()
+
+export const findById = (id) => prisma.alunos.findUnique({
+  where: { id: parseInt(id) }
+})
+
+export const create = (data) => prisma.alunos.create({ data })
+
+export const update = (id, data) => prisma.alunos.update({
+  where: { id: parseInt(id) },
+  data
+})
+
+export const remove = (id) => prisma.alunos.delete({
+  where: { id: parseInt(id) }
+})
