d34d7c0d9133912abf70a2bc71d5c4310b54ea4d
Luskahz <lucasalvesdemelolins@gmail.com>
2025-05-18 22:52:52 -0300

    finalização dos models, sem legendas no zod, apenas estrutura, faltando ajuste fino, inicio da criação dos controllers do periodo

--- src/controllers/periodo/createPeriodoController.js
+++ src/controllers/periodo/createPeriodoController.js
@@ -1,3 +1,23 @@
-export default function createPeriodoController(req, res){
-    const periodo = req.body
+import { create, periodoValidator } from "../../model/periodoModel.js"
+
+export default async function createPeriodoController(req, res, next){
+    try{
+        const periodo = req.body
+        const {success, error, data: periodoValidated} = periodoValidator(periodo, {id: true})
+        if(!success){
+            return res.status(400).json({
+                message: "erro ao iniciar periodo, verifique os dados!",
+                errors: error.flatten().fieldErrors
+            })
+        }
+    const result = await create(periodoValidated)
+    return res.json({
+        message: "Periodo iniciado com sucesso!",
+        periodo: result
+    })
+    } catch(error){
+        next(error)
+    }
+    
+
 }
