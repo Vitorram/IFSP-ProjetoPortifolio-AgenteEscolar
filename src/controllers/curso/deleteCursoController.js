ddbdddc42e513ba499fbc674293380ba7f6143d6
Luskahz <lucasalvesdemelolins@gmail.com>
2025-05-18 23:42:54 -0300

    crud curso completo

--- src/controllers/curso/deleteCursoController.js
+++ src/controllers/curso/deleteCursoController.js
@@ -1,3 +1,29 @@
-export default function deleteCursoController(){
+import {remove, cursoValidator} from '../../model/cursoModel'
+
+export default async function deleteCursoController(req, res, next){
+    try{
+        const {id} = req.params
+        const user = {
+            id: +id
+        }
+        const {success, error, data} = cursoValidator(user, {ano: true})
+        if(!success){
+            return res.status(400).json({
+                message: 'Erro ao excluir o curso, verifique os dados',
+                errors: error.flatten().fieldErrors
+            })
+        }
+        const result = await remove(data.id)
+        return res.json({
+            message: 'Curso excluido com sucesso',
+            Curso: result
+        })
+    } catch(error){
+        if(error?.code === 'P2025' && error?.meta?.cause.includes('Record to delete does not exist')){
+            return res.status(404).json({
+                message: 'Curso não encontrado!',
+            })
+        }
+    }
 
 }
