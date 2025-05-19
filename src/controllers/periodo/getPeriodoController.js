bbe1b82b73bb32cb246009d861e8c26bdd218558
Luskahz <lucasalvesdemelolins@gmail.com>
2025-05-18 23:33:49 -0300

    crud periodo finalizado, iniciando crud curso

--- src/controllers/periodo/getPeriodoController.js
+++ src/controllers/periodo/getPeriodoController.js
@@ -1,9 +1,15 @@
-export function getListPeriodoController(){
+import { getList } from "../../model/periodoModel"
+
+export async function getListPeriodoController(req, res, next){
+    try{
+        const result = await getList()
+        return res.json({
+            message: 'Lista de periodos',
+            periodos: result
+        })
+    } catch(error){
+        next(error)
+    }
 
 }
-
-export function getPeriodoController(){
-const {id} = req.params
-}
-
 
